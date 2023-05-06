9012f0d8e23783ffa54cb17cb9e820aef9959bed
Mauricio Urrego <mauricio@dotdotdot.email>
2023-05-06 20:03:51 +0200

    Adds easier to understand error handling in case .env is missing.

--- main.php
+++ main.php
@@ -10,9 +10,13 @@ use Symfony\Component\Config\FileLocator;
 require_once 'vendor/autoload.php';
 
 // Setting up .env file.
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-$dotenv->required('POSITION_STACK_API_KEY');
+try {
+	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+	$dotenv->load();
+	$dotenv->required(['POSITION_STACK_API_KEY', 'POSITION_STACK_API_URL']);
+} catch(\Exception $e) {
+	throw new \RuntimeException('Make sure to add a .env file to the root folder with POSITION_STACK_API_KEY and POSITION_STACK_API_URL variables');
+}
 
 // Setting up dependency injection.
 $container = new ContainerBuilder();
@@ -20,15 +24,16 @@ $loader = new YamlFileLoader($container, new FileLocator(__DIR__));
 try {
 	$loader->load('services.yaml');
 } catch (\Exception $e) {
-	echo 'Failed loading services.yaml with exception: ' . $e->getMessage();
+	throw new \RuntimeException('Failed loading services.yaml');
 }
 try {
 	$distanceCalculatorFromCsv = $container->get(CalculateDistancesFromCsvInterface::class);
 } catch (\Exception $e) {
-	echo 'Failed getting container with exception: ' . $e->getMessage();
+	throw new \RuntimeException('Failed getting container.');
 }
 
 echo 'Working...';
+echo PHP_EOL;
 $start = microtime(true); // To output script execution time.
 
 $distanceCalculatorFromCsv->calculateDistancesFromCsv(
@@ -37,6 +42,8 @@ $distanceCalculatorFromCsv->calculateDistancesFromCsv(
 );
 
 $end = microtime(true);
+echo PHP_EOL;
+echo 'Success!';
 echo PHP_EOL;
 echo 'Execution time: ' . round($end - $start, 2) . ' seconds.';
 echo PHP_EOL;
